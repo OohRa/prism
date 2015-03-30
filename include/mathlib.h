6701298f415d8707f3cd725b8c7a1afa4e94f423
Chris Moutsos <ckm13d@my.fsu.edu>
2015-03-30 18:01:16 -0400

    Fixed typedef

--- include/mathlib.h
+++ include/mathlib.h
@@ -3,7 +3,7 @@
 #define MATHLIB_H
 
 typedef double vec_t;
-typedef vec_t vec3_t[3];
+typedef vec_t[3] vec3_t;
 
 #define PI 3.1415926535897932
 
@@ -11,7 +11,6 @@ double abs( double num );
 double pow( double num, int exp );
 double sqrt( double num );
 int numDigits(double num);
-
 
 //Vector Functions
 void vecAdd( vec3_t c, const vec3_t a, const vec3_t b );
