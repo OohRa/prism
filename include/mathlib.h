1ac24499b8a701422373ffaf2290a2496ab2fdac
DylanJamesKolbBond <djk13b@my.fsu.edu>
2015-03-30 13:25:57 -0400

    Added functions: sqrt, vecAdd, vecSub, dotProduct, crossProduct, magnitude. Not tested\!

--- include/mathlib.h
+++ include/mathlib.h
@@ -2,7 +2,17 @@
 #ifndef MATHLIB_H
 #define MATHLIB_H
 
+typedef double vec_t;
+typedef vec_t vec3_t[3];
+
 double absol( double num );
 double power( double num, int exp );
+
+//Vector Functions
+void vecAdd( vec3_t c, vec3_t a, vec3_t b );
+void vecSub( vec3_t c, vec3_t a, vec3_t b );
+double dotProduct( vec3_t a, vec3_t b );
+void crossProduct( vec3_t c, vec3_t a, vec3_t b );
+double magnitude( vec3_t a );
 
 #endif 
--- src/mathlib.cpp
+++ src/mathlib.cpp
@@ -18,8 +18,74 @@ double absol( double num ){
 
 // Only allows integer exponents
 double power( double num, int exp ){
-	//Positive exponents
-	for( int i = 0; i < exp; ++i ){
-		num *= num;
+	int a_exp = absol( exp );
+	int temp = num;
+
+	if( exp == 0 )
+		return 1;
+	
+	else
+		for( int i = 0; i < a_exp; ++i ){
+			num *= temp;
+		}
+
+	return (exp < 0) ? (1 / num): num;
+}
+
+double sqrt( double num ){
+	int temp = 0;
+	
+	//Find power of ten
+	for( int i = 1; i != 0; ++i ){
+		if( num / power(10,i) < 1 )
+			break;
+		else  
+			temp = power( i + 1, 2 );	
 	}
+
+	//Search for roots
+	int flag = 0;
+	while( i != 0 ){
+		if( (temp * temp) > num ){
+			--temp;
+			++flag;
+		}
+		else if( (temp * temp) < num ){
+			++temp;
+			--flag;
+		}
+
+		if( flag == 0 )
+			break;
+	}	
+
+	return temp;
+}
+
+//Vector Functions
+void vecAdd( vec3_t c, vec3_t a, vec3_t b ){
+	c[0] = a[0] + b[0];
+	c[1] = a[1] + b[1];
+	c[2] = a[2] + b[2];
+
+} 
+
+void vecSub( vec3_t c, vec3_t a, vec3_t b ){
+	c[0] = a[0] - b[0];
+	c[1] = a[1] - b[1];
+	c[2] = a[2] - b[2];
+} 
+
+double dotProduct( vec3_t a, vec3_t b ){
+	return ( a[0] * b[0] + a[1] * b[1] + a[2] * b[2] );	
+}
+
+void crossProduct( vec3_t c, vec3_t a, vec3_t b ){
+	c[0] =  ( (a[1] * b[2]) - (a[2] * b[1]) );
+	c[1] = -( (a[0] * b[2]) - (a[2] * b[0]) );
+	c[2] =  ( (a[0] * b[1]) - (a[1] * b[0]) );
+}
+
+double magnitude( vec3_t a ){
+	return sqrt( (a[0] * a[0] ) + (a[1] * a[1]) + (a[2] * a[2]) );
 }
