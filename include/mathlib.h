03e33d07082d10500bbd4465fc64c630dc9fb29f
Chris Moutsos <ckm13d@my.fsu.edu>
2015-03-30 17:43:01 -0400

    Updated prototypes

--- include/mathlib.h
+++ include/mathlib.h
@@ -7,15 +7,17 @@ typedef vec_t vec3_t[3];
 
 #define PI 3.1415926535897932
 
-double absol( double num );
-double power( double num, int exp );
+double abs( double num );
+double pow( double num, int exp );
 double sqrt( double num );
+int numDigits(double num);
+
 
 //Vector Functions
-void vecAdd( vec3_t c, vec3_t a, vec3_t b );
-void vecSub( vec3_t c, vec3_t a, vec3_t b );
-double dotProduct( vec3_t a, vec3_t b );
-void crossProduct( vec3_t c, vec3_t a, vec3_t b );
-double magnitude( vec3_t a );
+void vecAdd( vec3_t c, const vec3_t a, const vec3_t b );
+void vecSub( vec3_t c, const vec3_t a, const vec3_t b );
+double dotProduct( const vec3_t a, const vec3_t b );
+void crossProduct( vec3_t c, const vec3_t a, const vec3_t b );
+double magnitude( const vec3_t a );
 
 #endif 
