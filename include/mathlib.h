b768f00f7416f0e92c091428a09b9f867cf39f13
DylanJamesKolbBond <djk13b@my.fsu.edu>
2015-03-30 13:27:37 -0400

    Created types for vectors and added functions definitions.

--- include/mathlib.h
+++ include/mathlib.h
@@ -7,6 +7,7 @@ typedef vec_t vec3_t[3];
 
 double absol( double num );
 double power( double num, int exp );
+double sqrt( double num );
 
 //Vector Functions
 void vecAdd( vec3_t c, vec3_t a, vec3_t b );
